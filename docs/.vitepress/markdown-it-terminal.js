222894e0fc939e24183092c976aa111f4b9f4bf7
Bairui Su <subairui@icloud.com>
2025-03-03 18:13:34 -0500

    Add run button (#163)

--- docs/.vitepress/markdown-it-terminal.js
+++ docs/.vitepress/markdown-it-terminal.js
@@ -1,6 +1,11 @@
 import container from "markdown-it-container";
 import {spawnSync} from "child_process";
 
+function codeof(token) {
+  if (token.type !== "fence" || token.tag !== "code") throw new Error("missing fenced code block");
+  return token.content;
+}
+
 export default function terminal(md) {
   md.use(container, "python", {
     render(tokens, idx) {
@@ -8,15 +13,18 @@ export default function terminal(md) {
       if (tokens[idx].nesting === -1) {
         const token = tokens[idx - 1]; // the code block
         const directives = tokens[idx - 2].info.split(/\s+/).slice(1);
-        if (token.type !== "fence" || token.tag !== "code") throw new Error("missing fenced code block");
-        const content = token.content;
+        const content = codeof(token);
         const process = spawnSync("python3", ["-c", content], {encoding: "utf8"});
         const output = process.stdout || process.stderr;
-        const safeOutput = md.utils.escapeHtml(output); 
+        const safeOutput = md.utils.escapeHtml(output);
+
         return `<Terminal ${directives.includes("fit") ? "fit" : ""}><pre>\n${safeOutput}</pre></Terminal>`;
       } else {
         // opening tag
-        return "";
+        const token = tokens[idx + 1]; // the code block
+        const content = codeof(token);
+        const link = "https://editor.gh2.dev/#code=" + encodeURIComponent(content);
+        return `<a class="gh-open no-icon" target="__blank" href="${link}" title="Run on Oneline Editor">Run</a>`;
       }
     },
   });
